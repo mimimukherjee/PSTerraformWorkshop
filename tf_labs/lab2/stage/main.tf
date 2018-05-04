4f0138021bbd01eef0948bb59cdd4ee6353da8f9
mimimukherjee <32750190+mimimukherjee@users.noreply.github.com>
2018-05-04 00:47:21 -0500

    clean up

--- tf_labs/lab2/stage/main.tf
+++ tf_labs/lab2/stage/main.tf
@@ -17,21 +17,6 @@ module "db" {
   env_prefix = "${var.env_prefix}"
 }
 
-/*
-module "webserver" {
-  source = "../modules/webserver"
-  tenancy_ocid = "${var.tenancy_ocid}"
-  compartment_ocid = "${var.compartment_ocid}"
-  ssh_public_key = "${var.ssh_public_key}"
-  ssh_private_key = "${var.ssh_private_key}"
-  WebSubnet_ocids = "${module.vcn.WebSubnet_ocids}"
-  InstanceShape = "${var.InstanceShape}"
-  WebServerCnt = "${var.WebServerCnt}"
-  SubnetCnt = "${var.SubnetCnt}"
-  env_prefix = "${var.env_prefix}"
-}
-*/
-
 module "nodejs" {
   source = "../modules/nodejs"
   tenancy_ocid = "${var.tenancy_ocid}"
@@ -56,11 +41,6 @@ module "lb" {
   WebServerCnt = "${var.WebServerCnt}"
   env_prefix = "${var.env_prefix}"
 
-}
-
-/*
-output "vcn_ocid" {
-  value ="${module.vcn.vcn_ocid}"
 }
 
 output "WebSubnet_ocids" {
